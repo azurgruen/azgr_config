3b431950e4dd1355fa4bf9067eed009d570815cc
Bernhard Schütz <schuetz@azurgruen.de>
2019-01-04 11:30:44 +0100

    8.x -> 9.x to avoid warnings when installing ext

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => null,
     'constraints'        => [
         'depends'   => [
-            'typo3' => '7.5.0-8.99.99'
+            'typo3' => '7.5.0-9.99.99'
         ],
         'conflicts' => [],
         'suggests'  => []
